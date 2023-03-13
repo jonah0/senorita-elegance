5afc3344c75c9dd7cdd188409b4adfb5fe3e939e
Jonah Stadtmauer <stadtmauer.j@northeastern.edu>
2023-03-13 16:47:50 +0100

    add barebones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [entries, setEntries] = useState([]);
+
+  function addEntry(entry) {
+    entries.push({ entry, timestamp: new Date() });
+    setEntries([...entries]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Señorita Elegance 🪱
+      <div className="button-div">
+        <button className="elegance-button" onClick={() => addEntry(0)}>
+          0
+        </button>
+        <button className="elegance-button" onClick={() => addEntry(1)}>
+          1
+        </button>
+        <button className="elegance-button" onClick={() => addEntry(2)}>
+          2
+        </button>
+      </div>
     </div>
   );
 }

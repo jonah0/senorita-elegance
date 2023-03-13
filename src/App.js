749b44f54ed952a1f2fbcfc116b7d26e0560ef7f
Jonah Stadtmauer <stadtmauer.j@northeastern.edu>
2023-03-13 17:41:17 +0100

    buttons add rows to datagrid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import { useState } from "react";
 import "./App.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { format } from "date-fns";
 
 function App() {
-  let [entries, setEntries] = useState([]);
+  let [counter, setCounter] = useState(0);
+  let [rows, setRows] = useState([]);
 
-  function addEntry(entry) {
-    entries.push({ entry, timestamp: new Date() });
-    setEntries([...entries]);
+  /**
+   *
+   * @param {number} phenotype `0`, `1`, or `2`
+   */
+  function addEntry(phenotype) {
+    let entry = { id: counter, entry: phenotype, timestamp: new Date() };
+    setCounter(counter + 1);
+    setRows([...rows, entry]);
   }
+
+  const columns = [
+    {
+      field: "entry",
+      headerName: "Entry",
+      type: "number",
+      width: 60,
+      editable: true,
+    },
+    {
+      field: "notes",
+      headerName: "Notes",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "timestamp",
+      headerName: "Timestamp",
+      type: "date",
+      valueFormatter: (params) => format(params.value, "yyyy-MM-dd HH:mm:ss"),
+      width: 200,
+    },
+  ];
 
   return (
     <div className="App">
@@ -22,6 +53,20 @@ function App() {
         <button className="elegance-button" onClick={() => addEntry(2)}>
           2
         </button>
+      </div>
+      <div style={{ height: 300, width: "100%" }}>
+        <DataGrid
+          initialState={{
+            sorting: {
+              sortModel: [{ field: "timestamp", sort: "desc" }],
+            },
+          }}
+          sx={{ height: "100%", fontFamily: "courier" }}
+          rows={rows}
+          columns={columns}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
       </div>
     </div>
   );

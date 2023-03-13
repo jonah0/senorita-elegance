9d9df0d692e6aa719e8071c5f440fd1af1f2242f
Jonah Stadtmauer <stadtmauer.j@northeastern.edu>
2023-03-13 18:40:00 +0100

    add gene field, delete button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import { useState } from "react";
 import "./App.css";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbar,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarDensitySelector,
+  GridToolbarExport,
+} from "@mui/x-data-grid";
 import { format } from "date-fns";
+import { TextField, FormControl, Button } from "@mui/material";
 
 function App() {
   let [counter, setCounter] = useState(0);
   let [rows, setRows] = useState([]);
+  let [gene, setGene] = useState("");
 
   /**
    *
    * @param {number} phenotype `0`, `1`, or `2`
    */
   function addEntry(phenotype) {
-    let entry = { id: counter, entry: phenotype, timestamp: new Date() };
+    let entry = {
+      id: counter,
+      phenotype,
+      gene,
+      timestamp: new Date(),
+    };
     setCounter(counter + 1);
     setRows([...rows, entry]);
   }
 
   const columns = [
     {
-      field: "entry",
-      headerName: "Entry",
+      field: "phenotype",
+      headerName: "Phenotype",
       type: "number",
-      width: 60,
+      width: 90,
       editable: true,
+    },
+    {
+      field: "gene",
+      headerName: "Gene",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "timestamp",
+      headerName: "Timestamp",
+      type: "date",
+      valueFormatter: (params) => format(params.value, "HH:mm:ss yyyy-MM-dd"),
+      width: 200,
     },
     {
       field: "notes",
@@ -32,13 +60,27 @@ function App() {
       editable: true,
     },
     {
-      field: "timestamp",
-      headerName: "Timestamp",
-      type: "date",
-      valueFormatter: (params) => format(params.value, "yyyy-MM-dd HH:mm:ss"),
-      width: 200,
+      field: "delete",
+      width: 75,
+      sortable: false,
+      disableColumnMenu: true,
+      renderHeader: () => {
+        return <button>test button</button>;
+      },
     },
   ];
+
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+        <GridToolbarExport />
+        <Button>Delete</Button>
+      </GridToolbarContainer>
+    );
+  }
 
   return (
     <div className="App">
@@ -54,18 +96,30 @@ function App() {
           2
         </button>
       </div>
-      <div style={{ height: 300, width: "100%" }}>
+      <div style={{ margin: "5px", textAlign: "left" }}>
+        <FormControl>
+          <TextField
+            variant="standard"
+            label="Gene"
+            value={gene}
+            onChange={(e) => setGene(e.target.value)}
+          ></TextField>
+        </FormControl>
+      </div>
+      <div className="data-grid-div">
         <DataGrid
           initialState={{
             sorting: {
               sortModel: [{ field: "timestamp", sort: "desc" }],
             },
           }}
+          slots={{ toolbar: CustomToolbar }}
           sx={{ height: "100%", fontFamily: "courier" }}
           rows={rows}
           columns={columns}
           checkboxSelection
           disableRowSelectionOnClick
+          autoHeight
         />
       </div>
     </div>
